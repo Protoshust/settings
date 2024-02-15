b1a6df14851d894e180b5caf34d6bf352a695a99
Alexey <protoshustt@gmail.com>
2024-02-16 01:28:25 +0400

    Fix
    16.02.24

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,4 @@
 oh-my-posh init pwsh | Invoke-Expression
 oh-my-posh init pwsh --config "~\Documents\settings\1_shell.omp.json" | Invoke-Expression
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
